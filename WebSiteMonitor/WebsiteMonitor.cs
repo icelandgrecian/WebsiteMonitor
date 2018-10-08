bf6b0e90f04226bc1012e0990c7bf833fa8ade64
Robert Thorne <robert.thorne@infomentor.co.uk>
2018-10-08 16:21:23 +0000

    Couple of tweaks to code

--- WebSiteMonitor/WebsiteMonitor.cs
+++ WebSiteMonitor/WebsiteMonitor.cs
@@ -17,11 +17,12 @@ namespace ConsoleApp3
     {        
         private int WAIT_TIME = int.Parse(ConfigurationManager.AppSettings["WaitTime"]);
         private static TimeSpan TIMEOUT_TIME = new TimeSpan(0, 0, int.Parse(ConfigurationManager.AppSettings["TimeoutSeconds"]));
+
         private string Website { get; set; }
 
         private static ConcurrentList<string> currentMessages = new ConcurrentList<string>();
         private static DateTime lastSend = DateTime.Now;
-        public readonly string outputName = "output " + Process.GetCurrentProcess().Id + " .txt";
+        private readonly string outputName = "output " + Process.GetCurrentProcess().Id + " .txt";
 
         public int timedOutLastTimeCount = 0;
 
@@ -45,126 +46,128 @@ namespace ConsoleApp3
             {
                 if (!SkipTimes.Any(f => f.IsInTime(DateTime.Now)))
                 {
-                    HttpClient c = new HttpClient();
-                    c.Timeout = TIMEOUT_TIME;
-
-                    StringBuilder consoleResult = new StringBuilder();
-
-                    consoleResult.Append(DateTime.Now + ": ");
-                    consoleResult.Append("Monitoring " + Website);
-
-                    try
+                    using (HttpClient c = new HttpClient())
                     {
-                        DateTime start = DateTime.Now;
-                        DateTime end = DateTime.Now;
+                        c.Timeout = TIMEOUT_TIME;
 
-                        using (HttpResponseMessage value = await c.GetAsync(Website))
+                        StringBuilder consoleResult = new StringBuilder();
+
+                        consoleResult.Append(DateTime.Now + ": ");
+                        consoleResult.Append("Monitoring " + Website);
+
+                        try
                         {
-                            end = DateTime.Now;
+                            DateTime start = DateTime.Now;
+                            DateTime end = DateTime.Now;
 
-                            if (!value.IsSuccessStatusCode)
+                            using (HttpResponseMessage value = await c.GetAsync(Website))
                             {
-                                switch (value.StatusCode)
+                                end = DateTime.Now;
+
+                                if (!value.IsSuccessStatusCode)
                                 {
-                                    case System.Net.HttpStatusCode.Forbidden:
+                                    switch (value.StatusCode)
+                                    {
+                                        case System.Net.HttpStatusCode.Forbidden:
 
-                                        consoleResult.Append(" (" + end.Subtract(start).TotalMilliseconds + " m/s)\n");
+                                            consoleResult.Append(" (" + end.Subtract(start).TotalMilliseconds + " m/s)\n");
 
-                                        break;
-                                    default:
-                                        consoleResult.Append("Bad Response Code  Connecting to " + Website + " " + value.StatusCode);
-                                        SendEmail(consoleResult, true);
+                                            break;
+                                        default:
+                                            consoleResult.Append("Bad Response Code  Connecting to " + Website + " " + value.StatusCode);
+                                            SendEmail(consoleResult, true);
 
-                                        break;
+                                            break;
+                                    }
                                 }
+                                else
+                                {
+                                    consoleResult.Append(" (" + end.Subtract(start).TotalMilliseconds + " m/s)\n");
+                                }
+                            }
+
+                            timedOutLastTimeCount = 0;
+                        }
+                        catch (TaskCanceledException ex)
+                        {
+                            if (timedOutLastTimeCount > 3)
+                            {
+                                consoleResult.Append("ERROR Timeout Connecting to " + Website + " (" + timedOutLastTimeCount.ToString() + " attempts) ");
+
+                                SendEmail(consoleResult, true);
                             }
                             else
                             {
-                                consoleResult.Append(" (" + end.Subtract(start).TotalMilliseconds + " m/s)\n");
+                                consoleResult.Append(" (Timeout)");
                             }
-                        }
 
-                        timedOutLastTimeCount = 0;
-                    }
-                    catch (TaskCanceledException ex)
-                    {
-                        if (timedOutLastTimeCount > 3)
+                            timedOutLastTimeCount++;
+                        }
+                        catch (Exception ex)
                         {
-                            consoleResult.Append("ERROR Timeout Connecting to " + Website + " (" + timedOutLastTimeCount.ToString() + " attempts) ");
+                            consoleResult.Append("Error Connecting to " + Website + " " + ex.ToString());
 
                             SendEmail(consoleResult, true);
-                        }
-                        else
-                        {
-                            consoleResult.Append(" (Timeout)");
+
+                            timedOutLastTimeCount = 0;
                         }
 
-                        timedOutLastTimeCount ++;
-                    }
-                    catch (Exception ex)
-                    {
-                        consoleResult.Append("Error Connecting to " + Website + " " + ex.ToString());
+                        Console.WriteLine(consoleResult);
 
-                        SendEmail(consoleResult, true);
-
-                        timedOutLastTimeCount = 0;
-                    }
-
-                    Console.WriteLine(consoleResult);
-
-                    try
-                    {
-                        File.AppendAllText(outputName, consoleResult.ToString());
-                    }
-                    catch
-                    {
-
-                        Console.WriteLine("Error Writing to File: " + outputName);
                         try
                         {
-                            File.AppendAllText(outputName + "backup", consoleResult.ToString());
+                            File.AppendAllText(outputName, consoleResult.ToString());
                         }
                         catch
                         {
-                            Console.WriteLine("Error Writing to Backup File: " + outputName + "backup");
-                        }
-                    }
 
-
-                    if (DateTime.Now.Subtract(lastSend).TotalMinutes > 2 && currentMessages.Any())
-                    {
-                        lock (currentMessages)
-                        {
-                            if (currentMessages.Any())
+                            Console.WriteLine("Error Writing to File: " + outputName);
+                            try
                             {
-                                MailMessage mail = new MailMessage(ConfigurationManager.AppSettings["EmailFrom"], ConfigurationManager.AppSettings["EmailTo"]);
-                                SmtpClient client = new SmtpClient();
-                                client.Port = 25;
-                                client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                                client.UseDefaultCredentials = false;
-                                client.Host = ConfigurationManager.AppSettings["SMTPServer"];
-                                mail.Subject = "Site Monitor Error";
-
-                                lock (currentMessages)
-                                {
-                                    StringBuilder sb = new StringBuilder();
-
-                                    foreach (string curmsg in currentMessages)
-                                    {
-                                        sb.AppendLine(curmsg);
-                                    }
-                                    mail.Body = sb.ToString() + "\n\r";
-
-                                    currentMessages.Clear();
-                                }
-
-                                client.Send(mail);
-
-                                lastSend = DateTime.Now;
+                                File.AppendAllText(outputName + "backup", consoleResult.ToString());
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Error Writing to Backup File: " + outputName + "backup");
                             }
                         }
-                    }
-                    Thread.Sleep(WAIT_TIME);
+
+
+                        if (DateTime.Now.Subtract(lastSend).TotalMinutes > 2 && currentMessages.Any())
+                        {
+                            lock (currentMessages)
+                            {
+                                if (currentMessages.Any())
+                                {
+                                    MailMessage mail = new MailMessage(ConfigurationManager.AppSettings["EmailFrom"], ConfigurationManager.AppSettings["EmailTo"]);
+                                    SmtpClient client = new SmtpClient();
+                                    client.Port = 25;
+                                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                                    client.UseDefaultCredentials = false;
+                                    client.Host = ConfigurationManager.AppSettings["SMTPServer"];
+                                    mail.Subject = "Site Monitor Error";
+
+                                    lock (currentMessages)
+                                    {
+                                        StringBuilder sb = new StringBuilder();
+
+                                        foreach (string curmsg in currentMessages)
+                                        {
+                                            sb.AppendLine(curmsg);
+                                        }
+                                        mail.Body = sb.ToString() + "\n\r";
+
+                                        currentMessages.Clear();
+                                    }
+
+                                    client.Send(mail);
+
+                                    lastSend = DateTime.Now;
+                                }
+                            }
+                        }
+                        Thread.Sleep(WAIT_TIME);
+                    }                   
                 }
                 else
                 {
